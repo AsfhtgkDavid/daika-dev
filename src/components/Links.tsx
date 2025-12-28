ed53df1f5287dfd6ffaa21a2741bc4343be5e5e4
AsfhtgkDavid <microdaika1@gmail.com>
2025-12-28 20:34:38 +0200

    fix[links]: fix link show on mobile

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -2,22 +2,23 @@ import DiscordIcon from "@/assets/discord-icon.svg";
 import GitHubIcon from "@/assets/github-mark-white.svg";
 import MailIcon from "@/assets/mail-icon.svg";
 import PyPIIcon from "@/assets/pypi-logo.svg";
-import CargoIcon from "/public/cargo.png";
+import CargoIcon from "/src/assets/cargo.png";
 import ArchIcon from "@/assets/arch-logo.svg";
 import CopyCard from "@/components/CopyCard.tsx";
 import LinkCard from "@/components/LinkCard.tsx";
+import { useIsMobile } from "@/hooks/useIsMobile.ts";
 
 const Links = () => {
 	return (
-		<div className="">
+		<div className="flex flex-col items-center justify-center">
 			<div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 p-4 md:p-8 w-133">
 				<LinkCard
 					title="GitHub"
 					description="My GitHub profile: projects, open-source contributions, and repositories."
 					href="https://github.com/AsfhtgkDavid"
 					icon={GitHubIcon}
-					className="col-span-full"
-					fullWidth
+					className="md:col-span-full"
+					fullWidth={!useIsMobile()}
 				/>
 				<LinkCard
 					title="PyPI"
@@ -32,15 +33,15 @@ const Links = () => {
 					icon={CargoIcon}
 				/>
 				<div className="col-span-full flex justify-center">
-				<LinkCard
-					title="AUR"
-					description="Packages I maintain for the Arch User Repository."
-					href="https://aur.archlinux.org/account/AsfhtgkDavid"
-					icon={ArchIcon}
-				/>
+					<LinkCard
+						title="AUR"
+						description="Packages I maintain for the Arch User Repository."
+						href="https://aur.archlinux.org/account/AsfhtgkDavid"
+						icon={ArchIcon}
+					/>
 				</div>
 			</div>
-			<hr />
+			<hr className="h-6 w-full" />
 			<div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 p-4 md:p-8">
 				<LinkCard
 					title="Mail"
