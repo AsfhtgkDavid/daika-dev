4387a5ea94ad04f0068c5d7d7b79c4a56ffee988
AsfhtgkDavid <daika@i.ua>
2026-01-29 16:15:06 +0200

    fix: Add logo preload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Links from "@/components/Links";
 import logo from "@/assets/logo.svg";
 import Typewriter from "@/components/Typewriter";
+import Image from "next/image";
 
 const Home = () => {
 	return (
 		<div className="flex items-center justify-center flex-col py-10">
-			{/* eslint-disable-next-line @next/next/no-img-element */}
-			<img src={logo.src} alt="logo" className="mb-24" />
+			<Image src={logo} alt="logo" className="mb-24" width="214.075" height="234.387" loading="eager"/>
 			<Typewriter headingText="Hi there, I'm David!" />
 			<Links />
 		</div>
