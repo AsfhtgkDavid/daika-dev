e53f9e6a89478fca48ace5fe71478f38368a07e9
David Lishchyshen <david@daika.dev>
2026-09-07 12:21:11 +0300

    fix: Make hr white

--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -42,7 +42,7 @@ export const Links = () => {
 					/>
 				</div>
 			</div>
-			<hr className="h-6 w-full" />
+			<hr className="h-6 w-full border-white" />
 			<div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 p-4 md:p-8">
 				<LinkCard
 					title="Mail"
